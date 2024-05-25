91d39c19a934497318e7e37a6770f1002663262d
jaianeoliveira581 <jaianeoliveira947@gmail.com>
2024-05-24 21:44:20 -0300

    versão 1.0

---
+++ main.py
@@ -0,0 +1,13 @@
+# crie um programa que tenha 
+'''
+Crie um programa que tenha as opções:
+ - Inserir pessoa na lista
+ - Listar pessoas cadastradas
+ - Pesquisar pelo nome de uma pessoa
+ - Ordenar a lista por ordem alfabética
+ - Atualizar nome
+ - Deletar nome da lista
+ - Sair do programa
+
+Ao terminar, crie um repositório local e um remoto, e envie o link do repositório do GitHub.
+'''
